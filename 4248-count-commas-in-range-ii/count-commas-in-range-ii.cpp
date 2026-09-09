f492adaaf2d17b11564752f2b2df2885899a3c69
Hiten Vashistha <hitenvashistha@gmail.com>
2026-09-09 08:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.33%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+
+        if (n >= 1000) {
+            ans += min(n, 999999LL) - 999;
+        }
+
+        if (n >= 1000000) {
+            ans += 2 * (min(n, 999999999LL) - 999999);
+        }
+
+        if (n >= 1000000000) {
+            ans += 3 * (min(n, 999999999999LL) - 999999999);
+        }
+
+        if (n >= 1000000000000LL) {
+            ans += 4 * (n - 999999999999LL);
+        }
+
+        if (n == 1000000000000000LL) {
+            ans++;
+        }
+
+        return ans;
+    }
+};
